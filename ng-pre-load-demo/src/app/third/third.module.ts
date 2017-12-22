46bf6290d5999ebb31a6f90c2c55ab00ae9a2a4d
louisxie <louisxie@truetel.com>
2017-12-22 11:39:27 +0800

    create pre load dmoe project.

---
+++ ng-pre-load-demo/src/app/third/third.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ThirdRoutingModule } from './third-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ThirdRoutingModule
+  ],
+  declarations: []
+})
+export class ThirdModule { }
