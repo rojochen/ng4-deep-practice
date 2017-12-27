29e9ac7a754ed0ce91591832ec49efb577649eb8
louisxie <louisxie@truetel.com>
2017-12-27 11:27:08 +0800

    Refactoring form

--- deep-app/src/app/form/check-tw-id/check-tw-id.component.ts
+++ deep-app/src/app/form/check-tw-id/check-tw-id.component.ts
@@ -9,35 +9,8 @@ import {
   Validators
 } from '@angular/forms';
 
-function hasExclamationMark(input: FormControl) {
-  const hasExclamation = input.value.indexOf('!') >= 0;
-  return hasExclamation ? null : { needsExclamation: true };
-}
-function CheckTaiwanID(input: FormControl) { // 身份證檢查函式
-  // console.log('input: ', input);
-  const userId = input.value;
-  const reg = /^[A-Z]{1}[1-2]{1}[0-9]{8}$/;  // 身份證的正規表示式
-  if (reg.test(userId)) {
-    const s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const ct = ['10', '11', '12', '13', '14', '15', '16', '17', '34', '18', '19', '20', '21',
-      '22', '35', '23', '24', '25', '26', '27', '28', '29', '32', '30', '31', '33'];
+import { hasExclamationMark, CheckTaiwanID } from '../validation';
 
-    const i = s.indexOf(userId.charAt(0));
-    const tempUserId = ct[i] + userId.substr(1, 9); // 若此身份證號若是A123456789=>10123456789
-    let num = Number(tempUserId.charAt(0)) * 1;
-    for (let x = 1; x <= 9; x++) {
-      num = num + Number(tempUserId.charAt(x)) * (10 - x);
-    }
-    num += Number(tempUserId.charAt(10)) * 1;
-    if ((num % 10) === 0) {
-      return null;
-    } else {
-      return { needsIdExclamation: true };
-    }
-  } else {
-    return { needsIdExclamation: true };
-  }
-}
 
 @Component({
   selector: 'app-check-tw-id',
