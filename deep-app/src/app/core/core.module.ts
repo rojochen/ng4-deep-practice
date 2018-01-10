803f41c56cc757ecb07f7d00c0cfc403dd8caf88
louisxie <louisxie@truetel.com>
2018-01-10 10:44:17 +0800

    fix environment .

--- deep-app/src/app/core/core.module.ts
+++ deep-app/src/app/core/core.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CoreRoutingModule } from './core-routing.module';
+
+// environment
+import { environment } from '../../environments/environment';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { Configuration } from './api/index';
 import { ConfigurationParameters } from './api/configuration';
+
+
+
 export function apiConfigFactory(): Configuration {
   const params: ConfigurationParameters = { username: '', password: '', apiKeys: {} };
   return new Configuration(params);
 }
 import { LoggerService } from './service/logger.service';
+
+console.log('environment: ', environment);
+
 @NgModule({
   imports: [
     CommonModule,
@@ -18,7 +27,7 @@ import { LoggerService } from './service/logger.service';
   ],
   providers: [
     LoggerService,
-    { provide: Configuration, useFactory: apiConfigFactory}
+    { provide: Configuration, useFactory: apiConfigFactory }
   ],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent]
