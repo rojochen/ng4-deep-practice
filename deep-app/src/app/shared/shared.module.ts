64e3485004e00b3efe3c08896038e27281af1f07
louisxie <louisxie@truetel.com>
2018-01-29 15:48:46 +0800

    form 表單驗證.

--- deep-app/src/app/shared/shared.module.ts
+++ deep-app/src/app/shared/shared.module.ts
@@ -7,13 +7,14 @@ import { DelayDirective } from './directive/delay.directive';
 import { BroadcasterService } from './service/broadcaster.service';
 import { SafePipe } from './pipe/safe.pipe';
 import { AlertMessageComponent } from './alert-message/alert-message.component';
+import { FieldErrorDisplayComponent } from './component/field-error-display/field-error-display.component';
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavLinkColorDirective, NameValidatorDirective, DelayDirective, SafePipe],
+  declarations: [NavLinkColorDirective, NameValidatorDirective, DelayDirective, SafePipe, FieldErrorDisplayComponent],
   exports: [
-    NavLinkColorDirective, NameValidatorDirective, DelayDirective, SafePipe
+    NavLinkColorDirective, NameValidatorDirective, DelayDirective, SafePipe, FieldErrorDisplayComponent
   ],
   providers: [
     BroadcasterService
