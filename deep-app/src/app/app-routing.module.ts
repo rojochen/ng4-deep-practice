1b2e19039617652ff7322da6670688149cf89ce9
RogerChen <jjc.roger@gmail.com>
2018-01-29 08:37:23 +0800

    added customPreload

--- deep-app/src/app/app-routing.module.ts
+++ deep-app/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Route, PreloadingStrategy, Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 // index component
 import { HomeComponent } from './home/home.component';
 import { DirectiveDemoComponent } from './directive-demo/directive-demo.component';
-
 import { DirectiveDemoModule } from './directive-demo/directive-demo.module';
+import { Observable } from 'rxjs/Observable';
 
 // 開啟Hash 模式需要import LocationStrategy & HashLocationStrategy from '@angular/common。
 const openHashConfig = { provide: LocationStrategy, useClass: HashLocationStrategy };
@@ -15,23 +15,29 @@ const appRoutes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'form', loadChildren: './form/form.module#FormModule' },
-    { path: 'optimize', loadChildren: './optimize/optimize.module#OptimizeModule' },
-    { path: 'component', loadChildren: './component/component.module#ComponentModule' },
+    { path: 'optimize', loadChildren: './optimize/optimize.module#OptimizeModule', data: { preload: true } },
+    { path: 'component', loadChildren: './component/component.module#ComponentModule', data: { preload: true } },
     { path: 'resolve', loadChildren: './route/route.module#RouteModule' },
     { path: 'directive', component: DirectiveDemoComponent },
     { path: '**', redirectTo: '' }
 ];
+export class AppCustomPreloader implements PreloadingStrategy {
+    preload(route: Route, load: Function): Observable<any> {
+        return route.data && route.data.preload ? load() : null;
+    }
+}
+// PreloadAllModules 預設全都preload , 只要在首頁執行時，有載入0.chunk.js就是會預先載入，如果不確定的話可以把預先載入的功能移除，二者交互比對就知道了。
 @NgModule({
     imports: [
         RouterModule.forRoot(appRoutes, {
-            preloadingStrategy: PreloadAllModules //只要在首頁執行時，有載入0.chunk.js就是會預先載入，如果不確定的話可以把預先載入的功能移除，二者交互比對就知道了。
+            preloadingStrategy: AppCustomPreloader
         }),
         DirectiveDemoModule
     ],
     declarations: [
     ],
     providers: [
-        openHashConfig
+        openHashConfig, AppCustomPreloader
     ],
     exports: [
         RouterModule
