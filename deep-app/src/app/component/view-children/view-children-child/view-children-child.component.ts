8079c2d0320335e8ec73070ff2a775551579aec5
Louis <neighborhood0830@gmail.com>
2018-01-31 00:14:51 +0800

    fix view-children component.

--- deep-app/src/app/component/view-children/view-children-child/view-children-child.component.ts
+++ deep-app/src/app/component/view-children/view-children-child/view-children-child.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-view-childer-child',
-  templateUrl: './view-childer-child.component.html',
-  styleUrls: ['./view-childer-child.component.css']
+  selector: 'app-view-children-child',
+  templateUrl: './view-children-child.component.html',
+  styleUrls: ['./view-children-child.component.css']
 })
-export class ViewChilderChildComponent implements OnInit {
+export class ViewChildrenChildComponent implements OnInit {
   @Input() ok = 'OK';
   @Output() close = new EventEmitter<Boolean>();
   hidden: Boolean = true;
